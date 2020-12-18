d30c4db6befe61068f11dcb65d9122e2dc6f9fed
Vdovichev <aspetin44@gmail.com>
2020-12-18 23:08:14 +0300

    Доработка заданий 2 и 5

--- Lesson_1/hw_1_2.py
+++ Lesson_1/hw_1_2.py
@@ -2,4 +2,4 @@ x = int(input('Введите время в секундах: '))
 hours = x // 3600
 minutes = (x // 60) % 60
 seconds = x % 60
-print(f'Время {hours}:{minutes}:{seconds}')
+print(f'Время {hours:02d}:{minutes}:{seconds}')
--- Lesson_1/hw_1_5.py
+++ Lesson_1/hw_1_5.py
@@ -1,13 +1,12 @@
 revenue = int(input('Введите значение выручки: '))
-cost: int =int(input('Введите значение издержек: '))
+cost: int = int(input('Введите значение издержек: '))
+profit = revenue - cost
 if revenue > cost:
     print('Фирма приносит прибыль')
-    profit = revenue - cost
     profitability = (profit / revenue) * 100
     print('Рентабельность в %: ', int(profitability))
+    people = int(input('Введите численность сотрудников фирмы: '))
+    people_profit = profit / people
+    print('Прибыль в расчёте на сотрудника: ', int(people_profit))
 else:
     print('Фирма несёт убыток')
-people = int(input('Введите численность сотрудников фирмы: '))
-profit = revenue - cost
-peopleprofit = profit / people
-print('Прибыль в расчёте на сотрудника: ', int(peopleprofit))
